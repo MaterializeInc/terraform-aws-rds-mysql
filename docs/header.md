924a53ee5fb793c11ed71a6952b7ba63354f0e60
Bobby Iliev <bobby@bobbyiliev.com>
2024-02-22 15:07:18 +0200

    Add examples

---
+++ docs/header.md
@@ -0,0 +1,7 @@
+# Materialize + MySQL RDS + Terraform
+
+Terraform module for deploying a new RDS MySQL instance and connecting it to Materialize.
+
+For the manual setup, see the [Materialize + RDS](TODO) documentation.
+
+> **Warning** This is provided on a best-effort basis and Materialize cannot offer support for this module.
--- examples/basic_usage/main.tf
+++ examples/basic_usage/main.tf
@@ -1,17 +1,23 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 module "my_rds_instance" {
   source = "../../"
 
-  rds_instance_name     = "test-rds-instance"
-  engine_version        = "8.0.23"
+  rds_instance_name     = "test-rds-mysql"
+  engine_version        = "8.0.36"
   rds_instance_class    = "db.t3.micro"
   publicly_accessible   = true
   mz_egress_ips         = var.mz_egress_ips
 }
 
 output "rds_instance_endpoint" {
-  value = module.my_rds_instance.rds_instance_endpoint
+  value = module.my_rds_instance.rds_instance
+  sensitive = true
+}
+
+output "mz_rds_mysql_details" {
+  value = module.my_rds_instance.mz_rds_mysql_details
+  sensitive = true
 }
